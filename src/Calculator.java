05fcfe3504134c8ace20bc61bb3aae8451638bd6
barbara-calle <barbara.calle@globant.com>
2022-04-19 21:07:55 -0500

    Rebasing the advance_calculator branch onto the base_calculator branch

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,6 +1,7 @@
 import java.util.Random;
 
 public class Calculator {
+<<<<<<< HEAD
     public static void sum(int number1, int number2){
         System.out.println(number1 + number2);
     }
@@ -17,5 +18,23 @@ public class Calculator {
 
         sum(number1, number2);
         subtract(number1, number2);
+=======
+    public static void multiplication(int number1, int number2){
+        System.out.println(number1 * number2);
+    }
+
+    public static void division(int number1, int number2){
+        System.out.println(number1 / number2);
+    }
+
+    public static void main(String[ ] args){
+        Random number = new Random();
+
+        int random_number1 = number.nextInt(100);
+        int random_number2 = number.nextInt(100);
+
+        multiplication(random_number1, random_number2);
+        division(random_number1, random_number2);
+>>>>>>> a2d4ec2 (Methods for multipication and division)
     }
 }
